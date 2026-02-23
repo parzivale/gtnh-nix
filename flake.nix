87f3b350ae108b1008e07967e066ddeae4175376
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-23 20:40:08 +0100

    added some basic nix config

---
+++ flake.nix
@@ -0,0 +1,138 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    haumea = {
+      url = "github:nix-community/haumea/v0.2.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {};
+      flake = module @ {
+        pkgs,
+        config,
+        lib,
+        ...
+      }: {
+        modules.nixos.gtnh = let
+          eulaFile = builtins.toFile "eula.txt" ''
+            # eula.txt managed by NixOS Configuration
+            eula=true
+          '';
+
+          serverPropertiesFile = serverConfig:
+            pkgs.writeText "server.properties"
+            (mkOptionText serverConfig);
+
+          encodeOptionValue = value: let
+            encodeBool = value:
+              if value
+              then "true"
+              else "false";
+            encodeString = value: builtins.escape [":" "=" "'"] value;
+            typeMap = {
+              "bool" = encodeBool;
+              "string" = encodeString;
+            };
+          in
+            (typeMap.${builtins.typeOf value} or toString) value;
+
+          mkOptionLine = name: value: let
+            dotNames = ["query-port" "rcon-password" "rcon-port"];
+            fixName = name:
+              if builtins.elem name dotNames
+              then
+                builtins.stringAsChars
+                (x:
+                  if x == "-"
+                  then "."
+                  else x)
+                name
+              else name;
+          in "${fixName name}=${encodeOptionValue value}";
+
+          mkOptionText = serverConfig: let
+            # Merge declared options with extraConfig
+            c =
+              (builtins.removeAttrs serverConfig ["extra-options"])
+              // serverConfig.extra-options;
+          in
+            builtins.concatStringsSep "\n"
+            (builtins.mapAttrsToList mkOptionLine c);
+
+          options = lib.mkOption {
+            type = lib.types.submodule {
+              options = inputs.haumea.lib.load {
+                src = ./options;
+                inputs = module;
+              };
+            };
+          };
+        in {
+          inherit options;
+          config.systemd.services.gtnh = {
+            description = "GTNH Server";
+            wantedBy = ["multi-user.target"];
+            after = ["network.target"];
+
+            path = with pkgs; [jre8 bash];
+
+            environment = {
+              # MCRCON_PORT = toString icfg.serverConfig.rcon-port;
+              MCRCON_PASS = "whatisloveohbabydonthurtmedonthurtmenomore";
+            };
+            script = '''';
+            serviceConfig = {
+              Restart = "always";
+              ExecStop = ''
+                ${pkgs.mcrcon}/bin/mcrcon stop
+              '';
+              TimeoutStopSec = "20";
+              User = "gtnh-user";
+              StateDirectory = "gtnh";
+              WorkingDirectory = "/var/lib/gtnh";
+            };
+
+            preStart = ''
+              # Ensure EULA is accepted
+              ln -sf ${eulaFile} eula.txt
+
+              # Ensure server.properties is present
+              if [[ -f server.properties ]]; then
+                mv -f server.properties server.properties.back
+              fi
+
+              # This file must be writeable, because Mojang.
+              cp ${serverPropertiesFile config.programs.gtnh.minecraft.server-properties} server.properties
+              chmod 644 server.properties
+            '';
+          };
+
+          users.users.gtnh-user = {
+            description = "GTNH service user";
+            isSystemUser = true;
+            group = "gtnh";
+            home = "/var/lib/gtnh";
+          };
+
+          users.groups.gtnh = {};
+
+          networking.firewall.allowedUDPPorts = "replaceme";
+          networking.firewall.allowedTCPPorts = "replaceme";
+        };
+      };
+    };
+}
