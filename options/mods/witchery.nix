f6e8b8be3e071ef648f493bfa48be1680c111d56
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-28 18:17:55 +0100

    fixed witchery

--- options/mods/witchery.nix
+++ options/mods/witchery.nix
@@ -5,14 +5,14 @@
     type = lib.types.submodule {
       options = {
         path = lib.mkOption {
-        type = lib.types.str;
-        default = "./config/witchery.cfg";
-        readOnly = true;
+          type = lib.types.str;
+          default = "./config/witchery.cfg";
+          readOnly = true;
         };
         kind = lib.mkOption {
-        type = lib.types.str;
-        default = "forge";
-        readOnly = true;
+          type = lib.types.str;
+          default = "forge";
+          readOnly = true;
         };
         general = lib.mkOption {
           default = {};
@@ -219,8 +219,8 @@
                 default = 4;
               };
               MantrapOpacity = lib.mkOption {
-                type = lib.types.str;
-                default = "0.3";
+                type = lib.types.float;
+                default = 0.3;
               };
               MirrorDimensionID = lib.mkOption {
                 type = lib.types.int;
@@ -228,15 +228,15 @@
               };
               MutandisAdditionalBlocks = lib.mkOption {
                 type = lib.types.listOf lib.types.str;
-                default = [ "witchery:glintweed,0" "tallgrass,2" ];
+                default = ["witchery:glintweed,0" "tallgrass,2"];
               };
               NaturesPowerReplaceableBlocks = lib.mkOption {
                 type = lib.types.listOf lib.types.str;
-                default = [ "mycelium" ];
+                default = ["mycelium"];
               };
               NewFairestOfThemAllSpawnChance = lib.mkOption {
-                type = lib.types.str;
-                default = "0.01";
+                type = lib.types.float;
+                default = 0.01;
               };
               PercentageOfPlayersSleepingForBuff = lib.mkOption {
                 type = lib.types.int;
@@ -284,7 +284,7 @@
               };
               StrawmanSpawnerRules = lib.mkOption {
                 type = lib.types.listOf lib.types.str;
-                default = [ "Zombie" "Zombie" "Skeleton" ];
+                default = ["Zombie" "Zombie" "Skeleton"];
               };
               TormentDimensionID = lib.mkOption {
                 type = lib.types.int;
@@ -344,7 +344,7 @@
               };
               TownBookshopAllowedBooks = lib.mkOption {
                 type = lib.types.listOf lib.types.str;
-                default = [ "book" "witchery:ingredient,46" "witchery:ingredient,47" "witchery:ingredient,48" "witchery:ingredient,49" "witchery:ingredient,81" "witchery:ingredient,106" "witchery:ingredient,107" "witchery:ingredient,127" "witchery:bookbiomes2" "witchery:cauldronbook" "Thaumcraft:ItemThaumonomicon" "TConstruct:manualBook" "TConstruct:manualBook,1" "TConstruct:manualBook,2" "TConstruct:manualBook,3" ];
+                default = ["book" "witchery:ingredient,46" "witchery:ingredient,47" "witchery:ingredient,48" "witchery:ingredient,49" "witchery:ingredient,81" "witchery:ingredient,106" "witchery:ingredient,107" "witchery:ingredient,127" "witchery:bookbiomes2" "witchery:cauldronbook" "Thaumcraft:ItemThaumonomicon" "TConstruct:manualBook" "TConstruct:manualBook,1" "TConstruct:manualBook,2" "TConstruct:manualBook,3"];
               };
               TownChurchClusterGroups = lib.mkOption {
                 type = lib.types.int;
@@ -515,8 +515,8 @@
                 default = 12;
               };
               VampireHunterSpawnChance = lib.mkOption {
-                type = lib.types.str;
-                default = "0.01";
+                type = lib.types.float;
+                default = 0.01;
               };
               WorldGenFrequency = lib.mkOption {
                 type = lib.types.int;
