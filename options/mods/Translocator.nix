eaab17f52cc5f2580b25cad85239340c7195a862
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-28 18:08:08 +0100

    fixed translocator

--- options/mods/Translocator.nix
+++ options/mods/Translocator.nix
@@ -5,14 +5,14 @@
     type = lib.types.submodule {
       options = {
         path = lib.mkOption {
-        type = lib.types.str;
-        default = "./config/Translocator.cfg";
-        readOnly = true;
+          type = lib.types.str;
+          default = "./config/Translocator.cfg";
+          readOnly = true;
         };
         kind = lib.mkOption {
-        type = lib.types.str;
-        default = "forge";
-        readOnly = true;
+          type = lib.types.str;
+          default = "forge_untyped";
+          readOnly = true;
         };
         checkUpdates = lib.mkOption {
           type = lib.types.str;
